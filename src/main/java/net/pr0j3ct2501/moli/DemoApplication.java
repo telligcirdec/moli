aa48ee622b68f0e550bdfa901557425a474e8ffb
telligcirdec <ced-public@gillets.net>
2019-06-28 21:36:42 +0200

    Initial commit

---
+++ src/main/java/net/pr0j3ct2501/moli/DemoApplication.java
@@ -0,0 +1,13 @@
+package net.pr0j3ct2501.moli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
